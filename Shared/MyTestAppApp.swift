c0f57318faf3a385e35fdaec997adaccd8dbc56d
Hoeeeeeh <hoeeeeeh@naver.com>
2022-06-25 12:34:33 +0900

    Initial Commit

---
+++ Shared/MyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestAppApp.swift
+//  Shared
+//
+//  Created by Hoeeeeeh on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
