96438f766de98187e2558e8a2a4829aee428523f
Hoeeeeeh <hoeeeeeh@naver.com>
2022-07-03 23:52:12 +0900

    Building Lists and Navigation

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let taskDateFormat : DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                .ignoresSafeArea()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading){
+                
+                Text("Turtle Rock")
+                    .foregroundColor(Color.black)
+                    .lineLimit(3)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    //.background(.yellow) // background Color
+                    //.lineSpacing(6) // 줄 간격
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                                        
+                    Spacer()
+
+                    Text("California")
+                                    
+                }.font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here")
+                
+            }.padding()
+            
+            Spacer()
+        }
     }
 }
 
---
+++ Shared/Landmark.swift
@@ -0,0 +1,16 @@
+//
+//  Landmark.swift
+//  MyTestApp
+//
+//  Created by Hoeeeeeh on 2022/06/30.
+//
+
+import Foundation
+
+struct Landmark : Hashable, Codable {
+    var id : Int
+    var name : String
+    var park : String
+    var state : String
+    var description : String
+}
