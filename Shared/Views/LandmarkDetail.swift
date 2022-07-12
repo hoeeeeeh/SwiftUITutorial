59ca14a6883e6a8b31f1955b55406ad4e5686b20
Hoeeeeeh <hoeeeeeh@naver.com>
2022-07-12 23:03:58 +0900

    Set Up Navigation Between List and Detail

---
+++ Shared/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  MyTestApp
+//
+//  Created by Hoeeeeeh on 2022/07/12.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
