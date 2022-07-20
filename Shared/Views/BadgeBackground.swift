fb978de8f9b2a399e35ed97570312bbc06745c6f
Hoeeeeeh <hoeeeeeh@naver.com>
2022-07-20 14:55:48 +0900

    Create Drawing Data for a Badge View

---
+++ Shared/Views/BadgeBackground.swift
@@ -0,0 +1,23 @@
+//
+//  BadgeBackground.swift
+//  MyTestApp
+//
+//  Created by Hoeeeeeh on 2022/07/20.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            
+        }
+        .fill(.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
